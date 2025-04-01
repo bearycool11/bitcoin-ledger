c0e959cb758fd022459892224c2a5ac9bb0c6e6e
J. K. Edwards <joed6834@colorado.edu>
2025-04-01 03:52:27 -0400

    Update grpcserver.h

--- src/rpc/grpcserver.h
+++ src/rpc/grpcserver.h
@@ -1,16 +1,21 @@
-// File: src/rpc/grpcserver.h
+/// File: src/rpc/grpcserver.h
+// Copyright (c) 2025
+// SPDX-License-Identifier: MIT
 
 #ifndef BITCOIN_GRPCSERVER_H
 #define BITCOIN_GRPCSERVER_H
 
 #include <memory>
-#include <thread>
-#include <grpcpp/grpcpp.h>
+#include <string>
 
 namespace grpcserver {
 
+// Forward declaration
 class GrpcServerImpl;
 
+/**
+ * @brief Encapsulates the gRPC server lifecycle.
+ */
 class GrpcServer {
 public:
     GrpcServer();
@@ -28,6 +33,13 @@ public:
 private:
     std::unique_ptr<GrpcServerImpl> impl;
 };
+
+/**
+ * @brief Global lifecycle helpers (for use in main.cpp or node glue code)
+ */
+void StartGRPCServer(const std::string& bind_address);
+void InterruptGRPCServer();
+void StopGRPCServer();
 
 } // namespace grpcserver
 
